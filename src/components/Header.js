d1a8363a06bbc79b0e0af6bd32fa1cc3a5d653c1
cordenbrock <cordenbrock@gmail.com>
2021-02-12 10:42:05 -0800

    Add container

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
 
 function Header() {
+  const tempStyle = {
+    backgroundColor: "#e28743",
+    textAlign: "center"
+  }
   return (
     <>
-    <h1>Tapper</h1>
+    <Container>
+      <h1 style={tempStyle}>Tapper</h1>
+    </Container>
+    <hr/>
+    <hr/>
     </>
   );
 }
