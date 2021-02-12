ab9487962e4d95569b691fd2561a0c16af0bd4d1
cordenbrock <cordenbrock@gmail.com>
2021-02-12 12:33:25 -0800

    Add methods and buttons for toggling form/list views and adding keg object from form

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import AddKeg from './AddKeg';
+import KegList from './KegList';
 
 class TapControl extends React.Component {
   constructor(props) {
@@ -19,12 +21,37 @@ class TapControl extends React.Component {
     }
   }
 
-  
+  handleVisiblePage = () => {
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage
+    }));
+  }
+
+  handleAddingKegToList = (newKeg) => {
+    this.setState({
+      masterTapList: [...this.state.masterTapList, newKeg],
+      formVisibleOnPage: false
+    })
+    console.log(this.state.masterTapList);
+  }
 
   render() {
+    let currentlyVisibleState = null;
+    let buttonText = null;
+
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <AddKeg onNewKeg={this.handleAddingKegToList} />
+      buttonText = "View Keg List";
+    } else {
+      currentlyVisibleState = <KegList kegList={this.masterTapList} />
+      buttonText = "Add Keg";
+    }
+
     return (
       <>
         <h2>Tap control</h2>
+        {currentlyVisibleState}
+        <button className={"btn-primary"} onClick={this.handleVisiblePage}>{buttonText}</button>
       </>
     );
   }
