5fe048c42c51a157aa875a6f7a4a4e91ac043af3
cordenbrock <cordenbrock@gmail.com>
2021-02-12 12:34:11 -0800

    fix indentation

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import KegControl from './KegControl';
-import Container from 'react-bootstrap/Container';
 
 function App(){
   
 
   return ( 
     <>
-      <Container fluid>
-        <Header />
-        <KegControl />   
-      </Container>
+      <Header />
+      <KegControl />
     </>
   );
 }
