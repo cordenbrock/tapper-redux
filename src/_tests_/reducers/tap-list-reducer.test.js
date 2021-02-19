f0f09d56a498c1ce1653210f80e2092622eaa73b
cordenbrock <cordenbrock@gmail.com>
2021-02-19 13:44:57 -0800

    Test return of default state with null action--good pass

--- src/_tests_/reducers/tap-list-reducer.test.js
+++ src/_tests_/reducers/tap-list-reducer.test.js
@@ -2,9 +2,28 @@ import tapListReducer from './../../reducers/tap-list-reducer';
 
 describe('tapListReducer', () => {
   
+  let action;
+  const tapList = [{
+    name: "Fizzbuzz Farmhouse Ale",
+    brand: "Gateway 2000 Fermentations",
+    price: "5.50",
+    alcoholContent: "6.2",
+    pintQuantity: "124",
+    id: '123'
+  },
+  {
+    name: "Lorem Ipsum Lager",
+    brand: "Drinkem Upsum Brewing",
+    price: "5.50",
+    alcoholContent: "7.7",
+    pintQuantity: "11",
+    id: '456'
+  }]
+
   test('Should return default state when action type is null', () => {
     expect(tapListReducer({}, {type: null})).toEqual({});
   });
+
 
 
   // test('Should do something', () => {
