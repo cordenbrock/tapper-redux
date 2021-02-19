e37eaf7f3ce92b8d42e3ec348c8d86e459440c18
cordenbrock <cordenbrock@gmail.com>
2021-02-19 15:15:51 -0800

    Write action return for ADD_TAP

--- src/reducers/tap-list-reducer.js
+++ src/reducers/tap-list-reducer.js
@@ -1,3 +1,18 @@
 export default (state = {}, action) => {
- return state;
+  const {  name, brand, price, alcoholContent, pintQuantity, id } = action;
+  switch (action.type) {
+  case 'ADD_TAP':
+    return Object.assign({}, state, {
+      [id]: {
+        name,
+        brand,
+        price,
+        alcoholContent,
+        pintQuantity,
+        id
+      }
+    });
+  default:
+    return state;
+  }
 };
