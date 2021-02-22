64bb7184b6f5a172405cf34d3e5d4a09f984c132
cordenbrock <cordenbrock@gmail.com>
2021-02-22 02:08:41 -0800

    Fix naming typo

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,12 @@
 import { combineReducers } from 'redux';
 import tapListReducer from './tap-list-reducer';
 import formVisibleReducer from './form-visible-reducer';
-import selectTapReducer from './selected-tap-reducer';
+import selectedTapReducer from './selected-tap-reducer';
 
 const rootReducer = combineReducers({
   masterTapList: tapListReducer,
   formVisibleOnPage: formVisibleReducer,
-  selectedTap: selectTapReducer
+  selectedTap: selectedTapReducer
 });
 
 export default rootReducer;
