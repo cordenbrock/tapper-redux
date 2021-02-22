8fc412954de2ad8bbb1a05dce6769c1c027155bb
cordenbrock <cordenbrock@gmail.com>
2021-02-21 21:26:46 -0800

    Write smoke tests to ensure root reducer connection to other reducers--good pass

--- src/_tests_/reducers/root-reducer.test.js
+++ src/_tests_/reducers/root-reducer.test.js
@@ -1,12 +1,24 @@
 import rootReducer from '../../reducers/root-reducer';
+import { createStore } from 'redux';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+import tapListReducer from '../../reducers/tap-list-reducer';
+
+let store = createStore(rootReducer);
 
 describe("rootReducer", () => {
 
-  test('Should return default state if no action type is recognized', () => {
+test('Should return default state if no action type is recognized', () => {
     expect(rootReducer({}, { type: null })).toEqual({
       masterTapList: {},
       formVisibleOnPage: false
     });
   });
 
+test('Check that initial state of tapListReducer matches root reducer', () => {
+    expect(store.getState().masterTapList).toEqual(tapListReducer(undefined, { type: null }));
+  });
+  
+test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
+  });
 });
