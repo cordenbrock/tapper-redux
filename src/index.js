3d117275ebb94357bc8c4e67febf5be094da966c
cordenbrock <cordenbrock@gmail.com>
2021-02-21 21:08:07 -0800

    Add root reducer to initial state and import

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import { createStore } from 'redux';
-import reducer from './reducers/tap-list-reducer';
+import rootReducer from './reducers/root-reducer';
 import { Provider } from 'react-redux';
 import { defaultTaps } from './defaultTaps/defaultTaps'
 
 const initialState = {
   masterTapList: {...defaultTaps}
 }
-const store = createStore(reducer, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(rootReducer, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <Provider store={store}>
