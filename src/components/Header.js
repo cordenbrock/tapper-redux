78d231ed78bdf61b3fadc919681fa3b878edbe19
Cory Nordenbrock <cordenbrock@gmail.com>
2022-01-26 11:54:49 -0600

    Change header bg-color

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Tapper from "./../img/tapper.jpg";
-// import * from "../../src/index.css"
 
 function Header() {
 
   return (
     <>
-    <Container fluid className="mb-3">
+    <Container fluid className="mb-3 header-bg">
       <img src={Tapper} alt="header banner from tapper arcade game" />
     </Container>
     </>
